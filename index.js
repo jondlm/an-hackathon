32df06925a6f4508498d0d0103b9e39eeea65f5b
Jon de la Motte <jdelamotte@appnexus.com>
2015-01-15 12:58:17 -0800

    More changes

--- index.js
+++ index.js
@@ -4,19 +4,51 @@
   var url = 'http://localhost:8081/';
   var languageCode = 'en';
   var sidebarUrl = chrome.extension.getURL('sidebar.html');
-  var fields = $('[data-tr-key]');
+
+  // var fields = $('[data-tr-key]');
 
   // Load sidebar
   $.get(sidebarUrl, function (sidebar) {
     document.body.innerHTML += sidebar;
+
+    var saveButton = $('.tr-save');
+
+    saveButton.on('click', function() {
+      var newKey = $('.tr-text-key').text();
+      var newValue = $('.tr-text-value').val();
+
+      $.ajax({
+        type: 'POST',
+        url: url + 'keys/' + newKey,
+        dataType: 'json',
+        data: {
+          language_code: languageCode,
+          value: newValue
+        },
+        success: function(){
+          // Preview the current page with the change
+          var justChanged = $("[data-tr-key='"+ newKey +"']");
+          justChanged.html(newValue);
+
+          reloadPhrases();
+        }
+      })
+
+    });
+
+    reloadPhrases();
+  });
+
+  // Completely reloads all phrases for the current language
+  function reloadPhrases () {
     var nav = $('.tr-translation-navigation');
-    $.getJSON(url + 'keys?language_code=en', function (items) {
+    nav.html('');
+    $.getJSON(url + 'keys?language_code=' + languageCode, function (items) {
       items.forEach(function (item) {
         nav.append(boxTemplate(item));
       });
     });
-
-  });
+  }
 
   function boxTemplate (data) {
     return '' +
@@ -27,4 +59,5 @@
       '</p>' +
     '</div>';
   }
+
 })(document, chrome, $);
