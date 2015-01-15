e9ca7524ef1c3adad6bae4d85374b7e04d527fc4
Joe Hsu <josephhsu@appnexus.com>
2015-01-15 10:51:13 -0800

    add sidebar and styles

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-(function(d) {
+(function(document, chrome, $) {
   'use strict';
 
+  var sidebarUrl = chrome.extension.getURL("sidebar.html");
   var fields = document.querySelectorAll('[data-tr-key]');
 
   Array.prototype.forEach.call(fields, function(x) {
     x.classList.add('to-edit');
   });
 
-
-})(document);
+  $.get(sidebarUrl, function(sidebar) {
+    document.body.innerHTML += sidebar;
+  });
+})(document, chrome, $);
